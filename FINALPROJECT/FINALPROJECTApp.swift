03e89441402d7f9f937263113042554be4e9375b
Kundana Addala <kaddala76@gmail.com>
2023-07-18 14:32:04 -0500

    Initial Commit

---
+++ FINALPROJECT/FINALPROJECTApp.swift
@@ -0,0 +1,17 @@
+//
+//  FINALPROJECTApp.swift
+//  FINALPROJECT
+//
+//  Created by Scholar on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct FINALPROJECTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
